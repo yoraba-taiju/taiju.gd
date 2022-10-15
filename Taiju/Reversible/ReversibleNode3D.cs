b57b8326de682184acc7c1fd1c5f1525eed3ab3b
Kaede Fujisaki <kaede@hexe.net>
2022-10-16 07:45:13 +0900

    Do not call base

--- Taiju/Reversible/ReversibleNode3D.cs
+++ Taiju/Reversible/ReversibleNode3D.cs
@@ -4,21 +4,18 @@ namespace Taiju.Reversible;
 
 public abstract partial class ReversibleNode3D : Node3D {
   public override void _Ready() {
-    base._Ready();
     OnReady();
   }
 
   protected abstract void OnReady();
 
   public override void _PhysicsProcess(double delta) {
-    base._PhysicsProcess(delta);
     OnPhysicsProcess(delta);
   }
 
   protected virtual void OnPhysicsProcess(double delta) {}
 
   public override void _Process(double delta) {
-    base._Process(delta);
     OnProcess(delta);
   }
 
