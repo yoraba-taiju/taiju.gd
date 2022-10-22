da93c62b3f84998cd9d97e84362577c7aefefb4d
Kaede Fujisaki <kaede@hexe.net>
2022-10-22 11:29:59 +0900

    Spawn system

---
+++ Taiju/Plugins/Timeline/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Godot;
+
+namespace Taiju.Plugins.Timeline; 
+
+public partial class Spawner : AnimationPlayer {
+  private Node3D field_;
+  public override void _Ready() {
+    field_ = GetNode<Node3D>("/root/Root/Field");
+    Play("Scene");
+  }
+  private void Invoke(PackedScene packed) {
+    var root = packed.Instantiate();
+    foreach(var child in root.GetChildren()){
+      root.RemoveChild(child);
+      field_.AddChild(child);
+    }
+  }
+}
