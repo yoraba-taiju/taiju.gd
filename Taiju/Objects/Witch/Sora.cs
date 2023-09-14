d2e2e84af62c238fdb4821f0f7dd5ff2e7005e78
Kaede Fujisaki <kaede@hexe.net>
2023-09-15 06:45:39 +0900

    Move!

--- Taiju/Objects/Witch/Sora.cs
+++ Taiju/Objects/Witch/Sora.cs
@@ -8,7 +8,24 @@ public partial class Sora : Node3D {
   public override void _Ready() {
   }
 
+  private const double MoveDelta = 30.0;
+
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(double delta) {
+    var pos = new Vector3();
+    if (Input.IsActionPressed("move_right")) {
+      pos.X += (float)(delta * MoveDelta);
+    }
+    if (Input.IsActionPressed("move_left")) {
+      pos.X -= (float)(delta * MoveDelta);
+    }
+    if (Input.IsActionPressed("move_up")) {
+      pos.Y += (float)(delta * MoveDelta);
+    }
+    if (Input.IsActionPressed("move_down")) {
+      pos.Y -= (float)(delta * MoveDelta);
+    }
+
+    Position += pos;
   }
 }
