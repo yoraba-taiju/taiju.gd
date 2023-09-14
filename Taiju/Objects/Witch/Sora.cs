c44159d2adef9c78ab3d3f5b7770f31cd94cd7fc
Kaede Fujisaki <kaede@hexe.net>
2023-09-14 22:33:22 +0900

    Remove old

--- Taiju/Objects/Witch/Sora.cs
+++ Taiju/Objects/Witch/Sora.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace Taiju.Objects.Witch;
@@ -9,7 +10,5 @@ public partial class Sora : Node3D {
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(double delta) {
-    var vel = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
-    Position = Position + new Vector3(vel.X, vel.Y, 0.0f);
   }
 }
