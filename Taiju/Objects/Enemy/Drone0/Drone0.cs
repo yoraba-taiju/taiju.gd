40bfb07f457745e7d5d7bf9281458aeb5ff643f4
Kaede Fujisaki <kaede@hexe.net>
2023-09-18 06:55:25 +0900

    Find sora

--- Taiju/Objects/Enemy/Drone0/Drone0.cs
+++ Taiju/Objects/Enemy/Drone0/Drone0.cs
@@ -1,18 +1,19 @@
+using System;
 using Godot;
 
 namespace Taiju.Objects.Enemy.Drone0;
 
 public partial class Drone0 : Node3D {
+  private Node3D sora_;
   // Called when the node enters the scene tree for the first time.
   public override void _Ready() {
     var player = GetNode<AnimationPlayer>("AnimationPlayer");
     var anim = player.GetAnimation("Rotate");
     anim.LoopMode = Animation.LoopModeEnum.Linear;
-    player.Play("Rotate");
+    sora_ = GetNode<Node3D>("/root/Root/Field/Witch/Sora");
   }
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(double delta) {
-    
   }
 }
