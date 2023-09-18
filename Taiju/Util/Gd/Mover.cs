3f868207c9bcc325e586cf82ad65f5067254d8b5
Kaede Fujisaki <kaede@hexe.net>
2023-09-19 00:28:04 +0900

    mover

---
+++ Taiju/Util/Gd/Mover.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+
+namespace Taiju.Util.Gd;
+
+public static class Mover {
+  public static Vector2 Follow(in Vector2 delta, in Vector2 velocity) {
+    return delta.Normalized() * velocity.Length();
+  }
+  
+  
+}
