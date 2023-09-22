acdf777d09dbf4895424649fac0a9a6b4b767c07
Kaede Fujisaki <kaede@hexe.net>
2023-09-22 18:42:14 +0900

    Playback

--- Taiju/Objects/Enemy/Drone0/Drone0.cs
+++ Taiju/Objects/Enemy/Drone0/Drone0.cs
@@ -10,6 +10,7 @@ public partial class Drone0 : Node3D {
     var anim = player.GetAnimation("Rotate");
     anim.LoopMode = Animation.LoopModeEnum.Linear;
     anim.RemoveTrack(anim.GetTrackCount() - 1);
+    player.PlaybackActive = true;
     player.Play("Rotate");
     sora_ = GetNode<Node3D>("/root/Root/Field/Witch/Sora");
   }
