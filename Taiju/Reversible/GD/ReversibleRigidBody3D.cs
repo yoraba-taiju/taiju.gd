907e0cd62e8a780dc8bc91840b86b43e154d666b
Kaede Fujisaki <kaede@hexe.net>
2023-09-27 20:26:46 +0900

    Remember

--- Taiju/Reversible/GD/ReversibleRigidBody3D.cs
+++ Taiju/Reversible/GD/ReversibleRigidBody3D.cs
@@ -4,11 +4,13 @@ namespace Taiju.Reversible.GD;
 
 public abstract partial class ReversibleRigidBody3D : RigidBody3D, IReversibleNode {
   private ClockNode clockNode_;
+  private Clock clock_;
 
-  protected Clock Clock => ClockNode.Clock;
   protected ClockNode ClockNode => clockNode_;
+  protected Clock Clock => clock_;
   
   public override void _Ready() {
     clockNode_ = GetNode<ClockNode>("/root/Root/Clock");
+    clock_ = clockNode_.Clock;
   }
 }
