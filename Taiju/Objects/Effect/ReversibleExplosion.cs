6a1f4192516ccc179cd4958ab6df01d999705c10
Kaede Fujisaki <kaede@hexe.net>
2023-10-10 21:44:34 +0900

    Use random hue

--- Taiju/Objects/Effect/ReversibleExplosion.cs
+++ Taiju/Objects/Effect/ReversibleExplosion.cs
@@ -5,24 +5,9 @@ using Taiju.Reversible.GD;
 namespace Taiju.Objects.Effect;
 
 public partial class ReversibleExplosion : ReversibleParticle3D {
-  [Export] private bool useRandomColor_;
+  [Export] private bool useRandomHue_;
   [Export] private Color color_ = Colors.Purple;
   private const float LifeTimeScale = 1.0f / 10.0f / 2.0f;
-  protected override void _Emit(ref Item[] items, double integrateTime) {
-    var rand = new RandomNumberGenerator();
-    var zero = Transform2D.Identity;
-    var maxSpeed = MaxSpeed;
-    var meshes = Meshes;
-    for (var i = 0; i < MeshCount; ++i) {
-      ref var item = ref items[i];
-      item.Color = useRandomColor_ ? Color.FromHsv(rand.Randf() * 360.0f, 0.5f, 0.9f) : color_;
-      item.Velocity = maxSpeed * (rand.Randf() / 2.0f + 0.5f);
-      item.Angle = new Vector2(rand.Randf() * 2.0f - 1.0f, rand.Randf() * 2.0f - 1.0f).Normalized();
-      item.LifeTime = item.Velocity * LifeTimeScale;
-      meshes.SetInstanceColor(i, item.Color);
-      meshes.SetInstanceTransform2D(i, zero);
-    }
-  }
 
   protected override void _Update(ref Item[] items, double integrateTime) {
     var meshes = Meshes;
@@ -44,6 +29,23 @@ public partial class ReversibleExplosion : ReversibleParticle3D {
         trans = trans.ScaledLocal(Vector2.One * (float)(leftTime / (lifeTime * 0.5f)));
       }
       meshes.SetInstanceTransform2D(i, trans);
+    }
+  }
+
+  protected override void _Emit(ref Item[] items, double integrateTime) {
+    var rand = new RandomNumberGenerator();
+    var zero = Transform2D.Identity;
+    var maxSpeed = MaxSpeed;
+    var meshes = Meshes;
+    color_.ToHsv(out _, out var saturation, out var value);
+    for (var i = 0; i < MeshCount; ++i) {
+      ref var item = ref items[i];
+      item.Color = useRandomHue_ ? Color.FromHsv(rand.Randf() * 360.0f, saturation, value) : color_;
+      item.Velocity = maxSpeed * (rand.Randf() / 2.0f + 0.5f);
+      item.Angle = new Vector2(rand.Randf() * 2.0f - 1.0f, rand.Randf() * 2.0f - 1.0f).Normalized();
+      item.LifeTime = item.Velocity * LifeTimeScale;
+      meshes.SetInstanceColor(i, item.Color);
+      meshes.SetInstanceTransform2D(i, zero);
     }
   }
 }
