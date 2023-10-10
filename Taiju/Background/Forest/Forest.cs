8eb5d03e137a7c718a3b16cd5636352a6a608d36
Kaede Fujisaki <kaede@hexe.net>
2023-10-10 20:04:02 +0900

    Update Forest

--- Taiju/Background/Forest/Forest.cs
+++ Taiju/Background/Forest/Forest.cs
@@ -1,18 +1,33 @@
 using Godot;
+using Taiju.Reversible.GD;
+using Taiju.Reversible.Value;
 
 namespace Taiju.Background.Forest;
 
-public partial class Forest : MeshInstance3D {
+public partial class Forest : ReversibleNode3D {
   private Material mat_;
 
   public override void _Ready() {
-    mat_ = GetActiveMaterial(0);
+    base._Ready();
+    var meshInstance = GetNode<MeshInstance3D>("Mesh");
+    mat_ = meshInstance.GetActiveMaterial(0);
   }
 
-  public override void _Process(double delta) {
+  public override bool _ProcessForward(double integrateTime, double dt) {
+    return false;
+  }
+
+  public override bool _ProcessBack() {
+    return false;
+  }
+
+  public override bool _ProcessLeap() {
+    return false;
+  }
+
+  public override void _ProcessRaw(double integrateTime) {
     var prop = mat_.Get("uv1_offset").AsVector3();
-    prop.X += (float)(0.1f * delta);
-    // prop.Y += (float)(0.0f * delta);
+    prop.X = (float)(0.1f * integrateTime);
     mat_.Set("uv1_offset", prop);
   }
 }
