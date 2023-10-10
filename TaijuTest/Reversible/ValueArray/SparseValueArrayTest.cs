f7f013288b6a6079c35a51d774d9da911956d5fe
Kaede Fujisaki <kaede@hexe.net>
2023-10-11 05:31:25 +0900

    SparseArray

---
+++ TaijuTest/Reversible/ValueArray/SparseValueArrayTest.cs
@@ -0,0 +1,10 @@
+﻿namespace TaijuTest.Reversible.ValueArray; 
+
+using Taiju.Reversible;
+using Taiju.Reversible.ValueArray;
+
+public class SparseValueArrayTest : AbstractValueArrayTest<SparseArray<int>> {
+  protected override SparseArray<int> Create(Clock clock, int initial) {
+    return new SparseArray<int>(clock, 2, initial);
+  }
+}
