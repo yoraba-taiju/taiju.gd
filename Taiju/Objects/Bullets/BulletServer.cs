1682ef32e2793b69b87c8705910a3b10bb5b9fd1
Kaede Fujisaki <kaede@hexe.net>
2023-10-13 11:06:14 +0900

    server

--- Taiju/Objects/Bullets/BulletServer.cs
+++ Taiju/Objects/Bullets/BulletServer.cs
@@ -1,5 +1,6 @@
 ﻿using Godot;
 using Taiju.Reversible.Gd;
+using Taiju.Reversible.ValueArray;
 
 namespace Taiju.Objects.Bullets;
 
@@ -7,6 +8,15 @@ public abstract partial class BulletServer : ReversibleNode3D {
   [Export] private Mesh mesh_;
   private MultiMeshInstance3D multiMeshInstance3D_;
   private MultiMesh multiMesh_;
+  private struct BulletState {
+    public bool Living;
+    public Vector3 Position;
+    public double SpawnAt;
+  }
+
+  private const int BulletCount = 256;
+
+  private SparseArray<BulletState> bullets_;
   public override void _Ready() {
     base._Ready();
     multiMeshInstance3D_ = GetNode<MultiMeshInstance3D>("Mesh");
@@ -14,5 +24,27 @@ public abstract partial class BulletServer : ReversibleNode3D {
     multiMeshInstance3D_.Multimesh = multiMesh_;
     multiMesh_.Mesh = mesh_;
     multiMesh_.UseColors = true;
+    multiMesh_.InstanceCount = BulletCount;
+    bullets_ = new SparseArray<BulletState>(Clock, BulletCount, new BulletState {
+      Living = false,
+      Position = Vector3.Zero,
+      SpawnAt = 0.0,
+    });
+  }
+
+  public override bool _ProcessForward(double integrateTime, double dt) {
+    return false;
+  }
+
+  public override bool _ProcessBack(double integrateTime) {
+    return false;
+  }
+
+  public override bool _ProcessLeap(double integrateTime) {
+    return false;
+  }
+
+  public override void _ProcessRaw(double integrateTime) {
+    base._ProcessRaw(integrateTime);
   }
 }
