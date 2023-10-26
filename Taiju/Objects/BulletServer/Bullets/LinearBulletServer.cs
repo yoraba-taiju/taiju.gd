d27602f842196fe1cb1c2940e2a4c959b866a3dd
Kaede Fujisaki <kaede@hexe.net>
2023-10-27 05:21:47 +0900

    Remove unused

--- Taiju/Objects/BulletServer/Bullets/LinearBulletServer.cs
+++ Taiju/Objects/BulletServer/Bullets/LinearBulletServer.cs
@@ -3,6 +3,4 @@
 namespace Taiju.Objects.BulletServer.Bullets; 
 
 public partial class LinearBulletServer: BulletServer<Linear> {
-  private double nextIntegrateTime_;
-  private RandomNumberGenerator rand_ = new();
 }
