8a1e5571ff9de088410f02da1c39a0b8798c4b4d
Kaede Fujisaki <kaede@hexe.net>
2023-10-27 05:13:50 +0900

    Fix

--- Taiju/Objects/BulletServer/Bullets/LinearBulletServer.cs
+++ Taiju/Objects/BulletServer/Bullets/LinearBulletServer.cs
@@ -1,7 +1,11 @@
-﻿using Taiju.Objects.BulletServer.Bullets;
+﻿using Godot;
 
 namespace Taiju.Objects.BulletServer.Bullets; 
 
 public partial class LinearBulletServer: BulletServer<Linear> {
-  
+  private double nextIntegrateTime_;
+  private RandomNumberGenerator rand_ = new();
+  public override void _ProcessRaw(double integrateTime) {
+    base._ProcessRaw(integrateTime);
+  }
 }
