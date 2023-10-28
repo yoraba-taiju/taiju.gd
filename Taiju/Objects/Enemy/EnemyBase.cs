a58ea5beeb88e485854f19392cc55218612e9517
Kaede Fujisaki <kaede@hexe.net>
2023-10-28 12:45:08 +0900

    Explosion

--- Taiju/Objects/Enemy/EnemyBase.cs
+++ Taiju/Objects/Enemy/EnemyBase.cs
@@ -1,23 +1,31 @@
 ﻿using Godot;
+using Taiju.Objects.Effect;
 using Taiju.Objects.Witch;
 using Taiju.Reversible.Gd;
 
 namespace Taiju.Objects.Enemy; 
 
 public abstract partial class EnemyBase : ReversibleRigidBody3D {
+  private PackedScene explosionScene_;
   protected Sora Sora { get; private set; }
 
   protected int Shield;
   public override void _Ready() {
     base._Ready();
+    explosionScene_ = ResourceLoader.Load<PackedScene>("res://Objects/Effect/RevesibleExplosion.tscn");
     Sora = GetNode<Sora>("/root/Root/Field/Witch/Sora");
     Shield = 1;
   }
 
   public void Hit() {
     Shield -= 1;
-    if (Shield <= 0) {
-      Destroy();
+    if (Shield > 0) {
+      return;
     }
+
+    Destroy();
+    var explosion = explosionScene_.Instantiate<ReversibleExplosion>();
+    explosion.Position = Position;
+    GetParent().AddChild(explosion);
   }
 }
