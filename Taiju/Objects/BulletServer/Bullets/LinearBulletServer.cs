13ff218d694e4fced5b75fa32189098269fcad34
Kaede Fujisaki <kaede@hexe.net>
2023-10-31 01:50:25 +0900

    use float

--- Taiju/Objects/BulletServer/Bullets/LinearBulletServer.cs
+++ Taiju/Objects/BulletServer/Bullets/LinearBulletServer.cs
@@ -4,8 +4,8 @@ using Taiju.Objects.Witch;
 namespace Taiju.Objects.BulletServer.Bullets; 
 
 public abstract partial class LinearBulletServer: BulletServer<Linear> {
-  public void SpawnToSora(Vector3 spawnAt, double speed) {
-    var velocity = (Sora.Position - spawnAt).Normalized() * (float)speed;
+  public void SpawnToSora(Vector3 spawnAt, float speed) {
+    var velocity = (Sora.Position - spawnAt).Normalized() * speed;
     var bullet = new Linear(new Vector2(spawnAt.X, spawnAt.Y), new Vector2(velocity.X, velocity.Y));
     Spawn(bullet);
   }
--- Taiju/Objects/Enemy/Drone0/Brain.cs
+++ Taiju/Objects/Enemy/Drone0/Brain.cs
@@ -70,7 +70,7 @@ public partial class Brain : EnemyBase {
         }
         else {
           state = State.Escape;
-          circleBulletServer_.SpawnToSora(pos, 15.0);
+          circleBulletServer_.SpawnToSora(pos, 15.0f);
         }
       }
         break;
