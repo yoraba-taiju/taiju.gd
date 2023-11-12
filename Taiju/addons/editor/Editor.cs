ace550b51eebe95499029860900900026674f2e5
Kaede Fujisaki <kaede@hexe.net>
2023-11-12 19:04:47 +0900

    Create plugin

---
+++ Taiju/addons/editor/Editor.cs
@@ -0,0 +1,16 @@
+#if TOOLS
+namespace Taiju.addons.editor;
+
+using Godot;
+
+[Tool]
+public partial class Editor : EditorPlugin {
+  public override void _EnterTree() {
+  }
+
+  public override void _ExitTree()
+  {
+  // Clean-up of the plugin goes here.
+  }
+}
+#endif
