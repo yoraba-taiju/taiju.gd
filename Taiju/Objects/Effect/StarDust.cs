ef7cd2fc1d06f77f32207cdc931db1d54e4ad789
Kaede Fujisaki <kaede@hexe.net>
2023-12-23 19:41:00 +0900

    Fix trail

--- Taiju/Objects/Effect/StarDust.cs
+++ Taiju/Objects/Effect/StarDust.cs
@@ -1,38 +1,46 @@
-﻿using Godot;
+﻿using System;
+using Godot;
 using Taiju.Objects.Reversible.Godot;
 
 namespace Taiju.Objects.Effect;
 
-public partial class StarDust : ReversibleParticle3D {
+public partial class StarDust : ReversibleParticle3D<StarDust.Item> {
+  public struct Item {
+    public Vector2 EmitPosition;
+    public Color Color;
+    public float Velocity;
+    public Vector2 Direction;
+    public double LifeTime;
+  }
   [Export] protected Color BaseColor = Colors.White;
   [Export(PropertyHint.Range, "0,360,")] protected float Angle;
   [Export(PropertyHint.Range, "0,180,")] protected float Range = 45;
   [Export] protected double DecayRate = 0.9;
 
   private RandomNumberGenerator rand_ = new();
-  protected override void _EmitOne(ref Item item, double integrateTime) {
+  protected override void _EmitOne(ref Item itemHolder) {
+    itemHolder.EmitPosition = new Vector2(GlobalPosition.X, GlobalPosition.Y);
     BaseColor.ToHsv(out _, out var saturation, out var value);
-    item.Color = Color.FromHsv(rand_.Randf() * 360.0f, saturation, value);
-    item.Velocity = MaxSpeed * (rand_.Randf() / 2.0f + 0.5f);
+    itemHolder.Color = Color.FromHsv(rand_.Randf() * 360.0f, saturation, value);
+    itemHolder.Velocity = MaxSpeed * (rand_.Randf() / 2.0f + 0.5f);
     var angle = Mathf.DegToRad(Angle - Range / 2 + rand_.Randf() * Range);
-    item.Direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-    item.LifeTime = 1.0;
+    itemHolder.Direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    itemHolder.LifeTime = 1.0;
   }
 
-  protected override bool _Update(ref readonly Item item, double integrateTime) {
-    var dt = integrateTime - item.EmitAt;
-    return dt <= item.LifeTime;
+  protected override bool _Update(ref readonly Item item, double t) {
+    return t <= item.LifeTime;
   }
 
-  protected override void _SetInstance(int i, ref readonly Item item, double integrateTime) {
-    var t = (float)(integrateTime - item.EmitAt);
+  protected override void _SetInstance(int i, ref readonly Item item, double t, Vector3 currentGlobalPosition) {
     var rate = t / item.LifeTime;
-    var alpha = 1.0f - (float)(rate);
+    var alpha = 1.0f - (float)rate;
 
     var color = item.Color;
     color.A *= alpha;
     Meshes.SetInstanceColor(i, color);
-    var trans = Transform2D.Identity.Translated(item.Direction * item.Velocity * t);
+    var delta = item.EmitPosition - new Vector2(currentGlobalPosition.X, currentGlobalPosition.Y);
+    var trans = Transform2D.Identity.Translated(delta + item.Direction * item.Velocity * (float)t);
     Meshes.SetInstanceTransform2D(i, trans);
   }
 }
--- Taiju/Objects/Reversible/Godot/ReversibleParticle3D.cs
+++ Taiju/Objects/Reversible/Godot/ReversibleParticle3D.cs
@@ -5,12 +5,14 @@ using Taiju.Objects.Reversible.ValueArray;
 namespace Taiju.Objects.Reversible.Godot;
 
 // https://docs.godotengine.org/en/stable/tutorials/performance/vertex_animation/controlling_thousands_of_fish.html
-public abstract partial class ReversibleParticle3D : ReversibleNode3D {
+public abstract partial class ReversibleParticle3D<T> : ReversibleNode3D
+  where T: struct
+{
   [Export] protected Mesh Mesh;
   [Export] protected int MeshCount = 16;
   [Export] protected float MaxSpeed = 10.0f;
   [Export(PropertyHint.Range, "1.0, 60.0")] protected double EmitPerSecond = 10;
-  private static readonly Transform2D TransNaN = new Transform2D().TranslatedLocal(new Vector2(Single.NaN, Single.NaN));
+  private readonly Transform2D transNaN_ = new Transform2D().TranslatedLocal(new Vector2(Single.NaN, Single.NaN));
 
   // https://docs.godotengine.org/en/stable/classes/class_multimesh.html
   private MultiMeshInstance3D multiMeshInstance_;
@@ -18,18 +20,15 @@ public abstract partial class ReversibleParticle3D : ReversibleNode3D {
   // MeshData
   protected MultiMesh Meshes { get; private set; }
 
-  protected struct Item {
+  struct ItemHolder {
     public bool Living;
     public double EmitAt;
-    public Color Color;
-    public float Velocity;
-    public Vector2 Direction;
-    public double LifeTime;
+    public T Item;
   }
 
   // Storages
   private ClockNode clockNode_;
-  private SparseArray<Item> items_;
+  private SparseArray<ItemHolder> items_;
   private double leftToEmit_;
 
   public override void _Ready() {
@@ -44,16 +43,16 @@ public abstract partial class ReversibleParticle3D : ReversibleNode3D {
     Meshes.InstanceCount = MeshCount;
     multiMeshInstance_.Multimesh = Meshes;
     multiMeshInstance_.Name = "SpritesNode";
-    items_ = new SparseArray<Item>(Clock, (uint)MeshCount, new Item());
+    items_ = new SparseArray<ItemHolder>(Clock, (uint)MeshCount, new ItemHolder());
     var span = items_.Mut;
-    _EmitOne(ref span[0], 0.0);
+    _EmitOne(ref span[0].Item);
     span[0].Living = true;
     span[0].EmitAt = 0.0;
   }
 
   public override bool _ProcessForward(double integrateTime, double dt) {
-    ReadOnlySpan<Item> span = items_.Ref;
-    Span<Item> spanMut = null;
+    ReadOnlySpan<ItemHolder> span = items_.Ref;
+    Span<ItemHolder> spanMut = null;
     leftToEmit_ -= dt;
     if (leftToEmit_ <= 0) {
       leftToEmit_ += 1.0 / EmitPerSecond;
@@ -64,7 +63,7 @@ public abstract partial class ReversibleParticle3D : ReversibleNode3D {
         }
         spanMut = spanMut != null ? spanMut : items_.Mut;
         ref var itemMut = ref spanMut[i];
-        _EmitOne(ref itemMut, integrateTime);
+        _EmitOne(ref itemMut.Item);
         itemMut.Living = true;
         itemMut.EmitAt = integrateTime;
         span = items_.Ref;
@@ -74,7 +73,7 @@ public abstract partial class ReversibleParticle3D : ReversibleNode3D {
 
     for (var i = 0; i < MeshCount; ++i) {
       ref readonly var item = ref span[i];
-      if (!item.Living || _Update(in item, integrateTime)) {
+      if (!item.Living || _Update(in item.Item, integrateTime - item.EmitAt)) {
         continue;
       }
       spanMut = spanMut != null ? spanMut : items_.Mut;
@@ -99,20 +98,21 @@ public abstract partial class ReversibleParticle3D : ReversibleNode3D {
     return true;
   }
 
-  private void SetInstances(ReadOnlySpan<Item> items, double integrateTime) {
+  private void SetInstances(ReadOnlySpan<ItemHolder> items, double integrateTime) {
+    var globalPosition = GlobalPosition;
     for (var i = 0; i < MeshCount; ++i) {
       ref readonly var item = ref items[i];
       if (!item.Living) {
-        Meshes.SetInstanceTransform2D(i, TransNaN);
+        Meshes.SetInstanceTransform2D(i, transNaN_);
         continue;
       }
-      _SetInstance(i, in item, integrateTime);
+      _SetInstance(i, in item.Item, integrateTime - item.EmitAt, globalPosition);
     }
   }
 
-  protected abstract void _EmitOne(ref Item item, double integrateTime);
+  protected abstract void _EmitOne(ref T item);
 
-  protected abstract bool _Update(ref readonly Item item, double integrateTime);
-  protected abstract void _SetInstance(int i, ref readonly Item item, double integrateTime);
+  protected abstract bool _Update(ref readonly T item, double t);
+  protected abstract void _SetInstance(int i, ref readonly T item, double t, Vector3 currentGlobalPosition);
 }
 
