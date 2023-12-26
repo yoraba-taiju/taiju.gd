91afde6baee4e3655070657031fd7f9e89da6e50
Kaede Fujisaki <kaede@hexe.net>
2023-12-26 21:07:03 +0900

    wakarase

--- Taiju/Objects/Effect/StarDust.cs
+++ Taiju/Objects/Effect/StarDust.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Godot;
+﻿using Godot;
 using Taiju.Objects.Reversible.Godot;
 
 namespace Taiju.Objects.Effect;
@@ -11,16 +10,23 @@ public partial class StarDust : ReversibleParticle3D<StarDust.Item> {
   [Export] protected double DecayRate = 0.9;
 
   public struct Item {
-    public Vector2 EmitPosition;
+    public Transform2D EmitTransform;
     public Color Color;
     public float Velocity;
     public Vector2 Direction;
     public double LifeTime;
   }
 
+  private Transform2D GetTransform2D() {
+    return new Transform2D(
+      GlobalTransform.Basis.X.X, GlobalTransform.Basis.X.Y,
+      GlobalTransform.Basis.Y.X, GlobalTransform.Basis.Y.Y,
+      GlobalTransform.Origin.X, GlobalTransform.Origin.Y);
+  }
+
   private RandomNumberGenerator rand_ = new();
   protected override void _EmitOne(ref Item item) {
-    item.EmitPosition = new Vector2(GlobalPosition.X, GlobalPosition.Y);
+    item.EmitTransform = GetTransform2D();
     BaseColor.ToHsv(out _, out var saturation, out var value);
     item.Color = Color.FromHsv(rand_.Randf() * 360.0f, saturation, value);
     item.Velocity = MaxSpeed * (rand_.Randf() / 2.0f + 0.5f);
@@ -40,8 +46,7 @@ public partial class StarDust : ReversibleParticle3D<StarDust.Item> {
     var color = item.Color;
     color.A *= alpha;
     Meshes.SetInstanceColor(i, color);
-    var baseDelta = item.EmitPosition - new Vector2(currentGlobalPosition.X, currentGlobalPosition.Y);
-    var trans = Transform2D.Identity.Translated(baseDelta + item.Direction * item.Velocity * (float)t);
+    var trans = (item.EmitTransform * GetTransform2D().Inverse()).Translated(item.Direction * item.Velocity * (float)t);
     Meshes.SetInstanceTransform2D(i, trans);
   }
 }
