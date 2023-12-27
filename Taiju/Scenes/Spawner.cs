01eb09ef10fc3d72a18be957550507e436f2d854
Kaede Fujisaki <kaede@hexe.net>
2023-12-27 12:28:11 +0900

    Remake

--- Taiju/Scenes/Spawner.cs
+++ Taiju/Scenes/Spawner.cs
@@ -7,8 +7,8 @@ public partial class Spawner : ReversibleSpawner {
   private void PlayRush(PackedScene packedScene) {
     var scene = packedScene.Instantiate<Node3D>();
     scene.QueueFree();
-    var all = scene.GetChildren();
-    foreach (var child in all) {
+    var children = scene.GetChildren();
+    foreach (var child in children) {
       scene.RemoveChild(child);
       Field.AddChild(child);
     }
