2dae6bbf2e5055e2c5ace6bee36231c91f9e2817
Kaede Fujisaki <kaede@hexe.net>
2023-12-28 12:13:00 +0900

    旋回

--- Taiju/Objects/Enemy/Drone1/Brain.cs
+++ Taiju/Objects/Enemy/Drone1/Brain.cs
@@ -7,7 +7,7 @@ using Taiju.Util.Godot;
 namespace Taiju.Objects.Enemy.Drone1;
 
 public partial class Brain : EnemyBase {
-  [Export(PropertyHint.Range, "0,360,")] private float maxRotateDegreePerSec_ = 120.0f;
+  [Export(PropertyHint.Range, "0,360,")] private float maxRotateDegreePerSec_ = 180.0f;
   [Export(PropertyHint.Range, "0,20,")] private float returnDistance_ = 12.0f;
   private const string SeekReq = "parameters/Seek/seek_request";
   private CircleBulletServer circleBulletServer_;
@@ -76,7 +76,11 @@ public partial class Brain : EnemyBase {
         var delta = soraPosition - currentPosition;
         var length = delta.Length();
         if (length < returnDistance_) {
-          rec.Velocity = Mover.Follow(Vector3.Right, rec.Velocity, maxAngle);
+          var sign = Mathf.Sign(delta.Y);
+          if (sign == 0) {
+            sign = defaultEscapeDirection_;
+          }
+          rec.Velocity = Vec.Rotate(rec.Velocity, sign * maxAngle) * Mathf.Exp((float)dt / 2);
         } else if (length > returnDistance_ * 1.1f) {
           circleBulletServer_.SpawnToSora(rec.Position, 15.0f);
           rec.State = State.Escape;
