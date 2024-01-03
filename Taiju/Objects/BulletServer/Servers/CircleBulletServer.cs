d0e729d4919d8f046b49e9afda0e7aa8b5c65a25
Kaede Fujisaki <kaede@hexe.net>
2024-01-03 10:35:07 +0900

    Renamed

--- Taiju/Objects/BulletServer/Servers/CircleBulletServer.cs
+++ Taiju/Objects/BulletServer/Servers/CircleBulletServer.cs
@@ -4,19 +4,19 @@ using Taiju.Objects.BulletServer.Bullets;
 namespace Taiju.Objects.BulletServer.Servers; 
 
 public partial class CircleBulletServer : LinearBulletServer {
-  [Export] private double hitSize_ = 0.2;
-  private double hitSizeSquared_;
+  [Export] private double hitRadius_ = 0.2;
+  private double hitRadiusSquared_;
 
   public override void _Ready() {
     base._Ready();
-    hitSizeSquared_ = hitSize_ * hitSize_;
+    hitRadiusSquared_ = hitRadius_ * hitRadius_;
   }
 
   protected override Response OnBulletMove(IBullet.Attitude attitude) {
     var soraPos = new Vector2(Sora.Position.X, Sora.Position.Y);
     var pos = attitude.Position;
 
-    if ((pos - soraPos).LengthSquared() <= hitSizeSquared_) {
+    if ((pos - soraPos).LengthSquared() <= hitRadiusSquared_) {
       return Response.HitToSora;
     }
 
