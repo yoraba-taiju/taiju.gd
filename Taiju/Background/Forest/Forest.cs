8994dddd29268c1c3b448d66cd7563cf19800609
Kaede Fujisaki <kaede@hexe.net>
2024-03-19 14:30:58 +0900

    Rename

--- Taiju/Background/Forest/Forest.cs
+++ Taiju/Background/Forest/Forest.cs
@@ -4,12 +4,12 @@ using Taiju.Objects.Reversible.Godot;
 namespace Taiju.Background.Forest;
 
 public partial class Forest : ReversibleNode3D {
-  private Material mat_;
+  private Material material_;
 
   public override void _Ready() {
     base._Ready();
     var meshInstance = GetNode<MeshInstance3D>("Mesh")!;
-    mat_ = meshInstance.GetActiveMaterial(0)!;
+    material_ = meshInstance.GetActiveMaterial(0)!;
   }
 
   public override bool _ProcessForward(double integrateTime, double dt) {
@@ -25,6 +25,6 @@ public partial class Forest : ReversibleNode3D {
   }
 
   public override void _ProcessRaw(double integrateTime) {
-    mat_.Set("uv1_offset", new Vector3((float)(0.1 * integrateTime), 0.0f, 0.0f));
+    material_.Set("uv1_offset", new Vector3((float)(0.1 * integrateTime), 0.0f, 0.0f));
   }
 }
