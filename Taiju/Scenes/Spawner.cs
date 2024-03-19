46f3343fea48e58c62d928c7774bdcc350904bc0
Kaede Fujisaki <kaede@hexe.net>
2024-03-19 10:47:52 +0900

    Null check

--- Taiju/Scenes/Spawner.cs
+++ Taiju/Scenes/Spawner.cs
@@ -3,17 +3,17 @@ using Godot;
 namespace Taiju.Scenes;
 
 public partial class Spawner : Node3D {
-  private Node3D Field { get; set; }
+  private Node3D field_;
 
   public override void _Ready() {
     base._Ready();
-    Field = GetNode<Node3D>("/root/Root/Field/Enemy");
+    field_ = GetNode<Node3D>("/root/Root/Field/Enemy")!;
   }
 
   private void Play(PackedScene packedNode, Vector2 at) {
     var node = packedNode.Instantiate<Node3D>();
     node.Translate(new Vector3(at.X, at.Y, 0.0f));
-    Field.AddChild(node);
+    field_.AddChild(node);
   }
 
   private void PlayRush(PackedScene packedScene) {
@@ -22,7 +22,7 @@ public partial class Spawner : Node3D {
     var children = rush.GetChildren();
     foreach (var child in children) {
       rush.RemoveChild(child);
-      Field.AddChild(child);
+      field_.AddChild(child);
     }
   }
 }
