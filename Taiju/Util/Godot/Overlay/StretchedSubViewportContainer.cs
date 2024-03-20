f65c1c3038ff6d3bb55623abf7cea8f300deabbb
Kaede Fujisaki <kaede@hexe.net>
2024-03-21 06:31:10 +0900

    rename

--- Taiju/Util/Godot/Overlay/StretchedSubViewportContainer.cs
+++ Taiju/Util/Godot/Overlay/StretchedSubViewportContainer.cs
@@ -7,14 +7,15 @@ public partial class StretchedSubViewportContainer : SubViewportContainer {
   public override void _Ready() {
     base._Ready();
     OnViewportSizeChanged();
-    GetParent().GetViewport().SizeChanged += OnViewportSizeChanged;
+    var viewPort = GetParent()!.GetViewport()!;
+    viewPort.SizeChanged += OnViewportSizeChanged;
   }
 
   private void OnViewportSizeChanged() {
     PivotOffset = Vector2.Zero;
     Position = Vector2.Zero;
-    var parent = GetParent()!.GetViewport()!;
-    Size = parent switch {
+    var viewPort = GetParent()!.GetViewport()!;
+    Size = viewPort switch {
       SubViewport s => s.Size,
       Window w => w.Size,
       _ => throw new ArgumentOutOfRangeException(),
