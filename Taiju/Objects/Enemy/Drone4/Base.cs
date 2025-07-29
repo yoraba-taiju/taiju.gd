788cf8674b3a52ca73dc1860ccac167a7e566a4a
Kaede Fujisaki <kaede@hexe.net>
2025-07-29 10:18:29 +0900

    Kamikaze attack

--- Taiju/Objects/Enemy/Drone4/Base.cs
+++ Taiju/Objects/Enemy/Drone4/Base.cs
@@ -1,5 +1,6 @@
 ﻿using Godot;
 using Taiju.Objects.Effect;
+using Taiju.Util;
 using Taiju.Util.Reversible.Value;
 
 namespace Taiju.Objects.Enemy.Drone4;
@@ -13,9 +14,16 @@ TParam: struct
   // Nodes
   protected AnimationPlayer AnimPlayer;
   protected StarDust StarDust;
+  
+  // Childs
+  protected Node3D Body;
 
   public struct RecordType {
     public int Shield;
+    public Vector3 Position;
+    public float Speed;
+    public Vector3 Direction;
+    public double Animation;
     public TParam Param;
   }
 
@@ -25,13 +33,51 @@ TParam: struct
     base._Ready();
     Record = new Dense<RecordType>(Clock, new RecordType {
       Shield = initialShield_,
+      Position = Position,
+      Speed = 0.0f, // Please set in subclass.
+      Direction = new Vector3(), // also
       Param = new TParam(),
     });
     AnimPlayer = GetNode<AnimationPlayer>("Body/Drone4/AnimationPlayer")!;
     AnimPlayer.CurrentAnimation = "Shaft_Rotate";
     StarDust = GetNode<StarDust>("Body/StarDust")!;
+    Body = GetNode<Node3D>("Body")!;
   }
-  
+
+  public override bool _ProcessForward(double integrateTime, double dt) {
+    ref var rec = ref Record.Mut;
+    { // Save Godot Status
+      rec.Position = Position;
+      rec.Animation = AnimPlayer.CurrentAnimationPosition;
+    }
+    { // Set Godot Status
+      Body.Rotation = new Vector3(0, 0, Vec.Atan2(-rec.Direction));
+    }
+    return base._ProcessForward(integrateTime, dt);
+  }
+
+  public override bool _ProcessBack(double integrateTime) {
+    LoadCurrentStatus(integrateTime);
+    return base._ProcessBack(integrateTime);
+  }
+
+  public override bool _ProcessLeap(double integrateTime) {
+    LoadCurrentStatus(integrateTime);
+    return base._ProcessLeap(integrateTime);
+  }
+
+  private void LoadCurrentStatus(double integrateTime) {
+    ref readonly var rec = ref Record.Ref;
+    Position = rec.Position;
+    AnimPlayer.Seek(rec.Animation);
+    Body.Rotation = new Vector3(0, 0, Vec.Atan2(-rec.Direction));
+  }
+
+  public override void _IntegrateForces(PhysicsDirectBodyState3D state) {
+    ref readonly var rec = ref Record.Ref;
+    state.LinearVelocity = rec.Direction.Normalized() * rec.Speed;
+  }
+
   protected override ref int ShieldMut => ref Record.Mut.Shield;
   
 }
--- Taiju/Objects/Enemy/Drone4/Kamikaze.cs
+++ Taiju/Objects/Enemy/Drone4/Kamikaze.cs
@@ -1,44 +1,121 @@
 ﻿using System;
 using Godot;
+using Taiju.Objects.Witch;
+using Taiju.Util;
 
 namespace Taiju.Objects.Enemy.Drone4;
 
 public partial class Kamikaze : Base<Kamikaze.Param> {
   [Export] private float initialSpeed_ = 10.0f;
+  [Export] private float kamikazeSpeed_ = 40.0f;
+  [Export(PropertyHint.Range, "0,360,")] private float maxRotateDegreePerSec_ = 60.0f;
+  [Export(PropertyHint.Range, "0,20,")] private float prepareDistance_ = 15.0f;
+  [Export(PropertyHint.Range, "0,20,")] private double timeOfPrepare_ = 1.0;
+  [Export(PropertyHint.Range, "0,20,")] private double timeOfPreKamikaze_ = 0.5;
+  [Export(PropertyHint.Range, "0,10,")] private float kamikazeStarDustSpeed_ = 25.0f;
 
   // Current State
-  private float angle_ = 0.0f;
+  private float initialStarDustSpeed_;
 
   // Param
   public enum State {
     Init,
+    Prepare,
+    PreKamikaze,
+    Kamikaze,
   }
   public struct Param {
     public State State;
-    public Vector3 Velocity;
-    public Vector3 Position;
-    public Vector3 Rotation;
+    public double TimeToNext;
   }
 
   // Nodes
 
   public override void _Ready() {
     base._Ready();
-    ref var param = ref Record.Mut.Param;
-    param.State = State.Init;
-    angle_ = 0.0f;
+    ref var rec = ref Record.Mut;
+    rec.Speed = initialSpeed_;
+    rec.Direction = Sora.Position - Position; // overwrite
+    ref var param = ref rec.Param;
+    param = new Param {
+      State = State.Init,
+      TimeToNext = 0.0,
+    };
+    initialStarDustSpeed_ = StarDust.MaxSpeed;
   }
 
   public override bool _ProcessForward(double integrateTime, double dt) {
-    ref var param = ref Record.Mut.Param;
+    ref var rec = ref Record.Mut;
+    ref var param = ref rec.Param;
+    var targetDirection = Sora.Position - Position;
     switch (param.State) {
       case State.Init: {
-        
+        rec.Direction = Mover.Follow(targetDirection, rec.Direction, maxRotateDegreePerSec_);
+        var distance = targetDirection.Length();
+        if (distance <= prepareDistance_) {
+          param.State = State.Prepare;
+          param.TimeToNext = timeOfPrepare_;
+
+          StarDust.Visible = false;
+          rec.Speed = 0.0f;
+        }
       }
+        break;
+      case State.Prepare:
+        rec.Direction = targetDirection;
+        param.TimeToNext -= dt;
+        if (param.TimeToNext < 0.0) {
+          param.State = State.PreKamikaze;
+          param.TimeToNext = timeOfPreKamikaze_;
+
+          StarDust.Visible = true;
+          StarDust.MaxSpeed = kamikazeStarDustSpeed_;
+          rec.Speed = 0.0f;
+        }
+        break;
+      case State.PreKamikaze:
+        param.TimeToNext -= dt;
+        if (param.TimeToNext < 0.0) {
+          param.State = State.Kamikaze;
+          StarDust.Visible = true;
+          rec.Speed = kamikazeSpeed_;
+        }
+        break;
+      case State.Kamikaze:
         break;
       default:
         throw new ArgumentOutOfRangeException();
     }
+
     return base._ProcessForward(integrateTime, dt);
+  }
+
+  public override bool _ProcessBack(double integrateTime) {
+    LoadCurrentStatus(integrateTime);
+    return base._ProcessBack(integrateTime);
+  }
+
+  private void LoadCurrentStatus(double integrateTime) {
+    ref readonly var rec = ref Record.Ref;
+    ref readonly var param = ref rec.Param;
+    switch (param.State) {
+      case State.Init:
+        StarDust.Visible = true;
+        StarDust.MaxSpeed = initialStarDustSpeed_;
+        break;
+      case State.Prepare:
+        StarDust.Visible = false;
+        StarDust.MaxSpeed = 0.0f;
+        break;
+      case State.PreKamikaze:
+        StarDust.Visible = true;
+        break;
+      case State.Kamikaze:
+        StarDust.Visible = true;
+        StarDust.MaxSpeed = kamikazeStarDustSpeed_;
+        break;
+      default:
+        throw new ArgumentOutOfRangeException();
+    }
   }
 }
