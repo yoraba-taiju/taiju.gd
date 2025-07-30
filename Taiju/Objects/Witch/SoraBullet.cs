06d8744385604bd47f29deb3406b2831d8034d46
Kaede Fujisaki <kaede@hexe.net>
2025-07-30 13:22:07 +0900

    Adjust

--- Taiju/Objects/Witch/SoraBullet.cs
+++ Taiju/Objects/Witch/SoraBullet.cs
@@ -50,7 +50,7 @@ public partial class SoraBullet : ReversibleRigidBody3D {
     ref var state = ref record_.Mut;
     ref var pos = ref state.Position;
     
-    if (Mathf.Abs(pos.X) >= 21.00001f || Mathf.Abs(pos.Y) >= 11.50001f) {
+    if (Mathf.Abs(pos.X) >= 20.3f || Mathf.Abs(pos.Y) >= 11.6f) {
       Destroy();
     }
 
