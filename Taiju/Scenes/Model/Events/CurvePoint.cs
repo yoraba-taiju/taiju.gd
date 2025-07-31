4dc5fc2733465e990223b0cf8230df51ab1ac0c2
Kaede Fujisaki <kaede@hexe.net>
2025-08-01 08:55:15 +0900

    Mark as struct

--- Taiju/Scenes/Model/Events/CurvePoint.cs
+++ Taiju/Scenes/Model/Events/CurvePoint.cs
@@ -2,7 +2,7 @@
 
 namespace Taiju.Scenes.Model.Events;
 
-public class CurvePoint {
+public struct CurvePoint {
   public struct Point {
     [JsonPropertyName("X")] public required float X { get; init; }
     [JsonPropertyName("Y")] public required float Y { get; init; }
